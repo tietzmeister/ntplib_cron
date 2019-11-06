ebef55e7c9e5f16b4ae00f8b35fc9e6105e88897
tietzmeister <tietzmeister@gmail.com>
2019-11-06 14:45:46 -0500

    Update correctsystemtime.py

--- correctsystemtime.py
+++ correctsystemtime.py
@@ -4,17 +4,16 @@ import time
 import os
 import sys
 
+# Append a system path to module for ntplib #
 sys.path.append("/usr/local/lib/python2.7/dist-packages/")
 
-#try:
-import ntplib
-client = ntplib.NTPClient()
-response = client.request('lovebug.pasco.k12.fl.us')
-response = time.strftime('%m%d%H%M%Y',time.localtime(response.tx_time))
-print response
-newtime = os.system('sudo date ' + response)
-print newtime
-#except:
-#    print('Could not sync with time server.')
-
-print('Done.')
+try:
+  import ntplib
+  client = ntplib.NTPClient()
+  response = client.request('pool.ntp.org')
+  response = time.strftime('%m%d%H%M%Y',time.localtime(response.tx_time))
+  print response
+  newtime = os.system('sudo date ' + response)
+  print newtime
+ except:
+  print('Could not update time')
